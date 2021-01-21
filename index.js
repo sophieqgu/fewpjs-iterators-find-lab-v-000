d0532161092d30558149f6fd04367bb61a7c569c
Sophie Gu <sophieqgu@gmail.com>
2021-01-21 04:57:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function testFunc() {
 }
 
 function superbowlWin(records) {
-  let win = records.find(record => record.result === "W");
+  let win = records.find( record => record.result === "W");
   if (win) {
     return win.year;
   }

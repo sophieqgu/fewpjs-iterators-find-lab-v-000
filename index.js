89282f63a852d1b5ab7f6007db05c84b9ddbdeac
Sophie Gu <sophieqgu@gmail.com>
2021-01-21 04:57:08 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(records) {
+  let win = records.find( record => record.result === "W");
+  if (win) {
+    return win.year;
+  }
+}
